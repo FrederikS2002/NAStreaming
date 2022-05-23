f26c7ba6c6b16407d6c016c7b08ebd874f623fcc
FrederikS2002 <86200874+FrederikS2002@users.noreply.github.com>
2022-05-23 17:51:47 +0200

    video player progress, pip, fullscreen

---
+++ frontend/src/routes/play/[...id].svelte
@@ -0,0 +1,13 @@
+<div>
+    <video src="https://www.w3schools.com/html/mov_bbb.mp4"></video>
+</div>
+<style lang="scss">
+    root {
+      width: 100%;
+      height: 100vh;
+    }
+    body {
+      width: 100%;
+      height: 100%;
+    }
+</style>
